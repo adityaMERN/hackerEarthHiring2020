c980a3ca91703f1f29ccddc0e29960549d54ad4c
Aditya anand <adityaanandofficial@gmail.com>
2020-12-31 12:36:17 +0530

    step1&step2 implemented

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+import data from './data';
+import { useEffect, useState } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
+function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  // useEffect(()=>{
+  //   const dataset=localStorage.getItem("my-cart")
+  //   if (dataset) {
+  //     setCartItems(JSON.parse(dataset))
+  //   }
+    
+  // },[]);
+  // useEffect(()=>{
+  //   localStorage.setItem("my-cart",JSON.stringify(cartItems))
+  // })
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      confirmAlert({
+        title: 'Item deleted',
+        buttons: [
+          {
+            label: 'OK',
+            onClick: setCartItems(cartItems.filter((x) => x.id !== product.id))
+          }
+        ]
+      });
+      
+    } else {
+      confirmAlert({
+        title: 'Item deleted',
+        buttons: [
+          {
+            label: 'OK',
+            onClick: setCartItems(
+              cartItems.map((x) =>
+                x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+              )
+            )
+          }
+        ]
+      });
+      
+    }
+  };
+ 
+  return (
+    <div className="App">
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row-main">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          
+        ></Basket>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// onRemove={onRemove} to delete
---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Product(props) {
+  const { product, onAdd } = props;
+  return (
+    <div>
+      <img className="small" src={product.img_url} alt={product.name} />
+      <h3>{product.name}</h3>
+      <div>${product.price}</div>
+      <div>
+        <button onClick={() => onAdd(product)}>Add To Cart</button>
+      </div>
+    </div>
+  );
+}
